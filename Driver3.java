5c241861a59ce9755c21043aaf58f9adfe399385
austin123 <zam449@hotmail.com>
2013-04-06 15:14:15 -0700

    should be able to compile with node class

--- Driver3.java
+++ Driver3.java
@@ -4,18 +4,19 @@
 
 	
    public class Driver3{
-      Node start, room = null;
+      Node start = null;
       private File txtFile;
       private String fileName, line;
       boolean kFound = false;
       int k = 1; 
-      int roomNum, scaryRoomNum = 0;
+      ArrayList<Integer> roomsWithBlockedWalls = new ArrayList<Integer>();
    	
       public void readFile(){
-      
-         int arrayPos = 0;
+         int scaryRoomNum = 0;
+         int arrayPos, roomNum = 0;
+         int room;
          String []stringsOfInt;
-         Node []nodeArray;
+         ArrayList<Node> nodeArray = new ArrayList<Node>();
          try{//choose a file
             JFileChooser aChooser = new JFileChooser();
             System.out.println("Please choose a .txt file");
@@ -23,38 +24,36 @@
             txtFile = aChooser.getSelectedFile();
             fileName = txtFile.getName();
             BufferedReader readFile = new BufferedReader(new FileReader(fileName));
+         	
             while ((line = readFile.readLine()) != null){
                stringsOfInt = line.split(" ");//needed to read diff rooms on same line
                if (roomNum < (k*k)){//initially one, but we get the real k later
                   if (kFound == false){//only gets the k val
-                     k = Integer.parseInt(stringsOfInt[1]);//get real k
-                     nodeArray = new nodeArray(k*k);
+                     k = Integer.parseInt(stringsOfInt[0]);//get real k
                      kFound = true;}
                   else{
                   //creates node and puts it into array
-                     nodeArray[roomNum] = new Node (Integer.parseInt(stringsOfInt[1]),roomNum);
+                     nodeArray.add(new Node (Integer.parseInt(stringsOfInt[0]),roomNum));
                      roomNum++;
                   }
+                  continue;
                }
-               if (scaryRoomNum < (k*k)){
-                  nodeArray[scaryRoomNum].setScary(Integer.parseInt(stringsOfInt[1]));
-                  nodeArray[scaryRoomNum].setWork(Integer.parseInt(stringsOfInt[1]));//TEMPORARY
+               else if (scaryRoomNum < (k*k)){
+                  nodeArray.get(scaryRoomNum).setSpookiness(Integer.parseInt(stringsOfInt[0]));
                   scaryRoomNum++;
+               
+               }
+               else{
+                  roomsWithBlockedWalls.add(Integer.parseInt(stringsOfInt[0]));
+                  roomsWithBlockedWalls.add(Integer.parseInt(stringsOfInt[1]));
                }
             }
-         
-            for (roomNum = 0; roomNum < (k*k); roomNum++){//gets shared walls
-               nodeArray[roomNum].sharesWallWith(roomNum + 1, k);
-               nodeArray[roomNum].sharesWallWtih(roomNum - 1, k);
-               nodeArray[roomNum].sharesWallWith(roomNum + k, k);
-               nodeArray[roomNum].sharesWallWith(roomNum + k - 1, k );
-               nodeArray[roomNum].sharesWallWith(roomNum - k, k);
-               nodeArray[roomNum].sharesWallWith(roomNum - k + 1, k);}
-                
+            for (roomNum = 0; roomNum < (k*k); roomNum++)//gets shared walls
+               nodeArray.get(scaryRoomNum).defaultRoomAccess(k, roomNum);					
          }
             catch (Exception e){System.err.println("Error " + e);}
       }
-   
+   	
       public static void main (String[] args) throws Exception {
          Driver3 d3 = new Driver3();
          d3.readFile();
