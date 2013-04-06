5bd10a8f102e71e2fa645b1944ce40fcd7a85764
austin123 <zam449@hotmail.com>
2013-04-06 11:57:47 -0700

    passed in the k

--- Driver3.java
+++ Driver3.java
@@ -2,17 +2,17 @@
    import java.io.*;//for writer
    import javax.swing.*;//for file chooser
 
-  
+	
    public class Driver3{
       Node start, room = null;
       private File txtFile;
       private String fileName, line;
       boolean kFound = false;
-      int k = -1; 
-      int roomNum = 0;
-		
+      int k = 1; 
+      int roomNum, scaryRoomNum = 0;
+   	
       public void readFile(){
-		
+      
          int arrayPos = 0;
          String []stringsOfInt;
          Node []nodeArray;
@@ -23,38 +23,37 @@
             txtFile = aChooser.getSelectedFile();
             fileName = txtFile.getName();
             BufferedReader readFile = new BufferedReader(new FileReader(fileName));
-            while ((line = readFile.readLine()) != null)
-               if (roomNum < (k*k)){
-                  stringsOfInt = line.split(" ");
-                  if (kFound == false){
-                     k = Integer.parseInt(stringsOfInt[1]);
+            while ((line = readFile.readLine()) != null){
+               stringsOfInt = line.split(" ");//needed to read diff rooms on same line
+               if (roomNum < (k*k)){//initially one, but we get the real k later
+                  if (kFound == false){//only gets the k val
+                     k = Integer.parseInt(stringsOfInt[1]);//get real k
                      nodeArray = new nodeArray(k*k);
                      kFound = true;}
                   else{
-                     nodeArray[roomNum] = new Node (Integer.parseInt(stringsOfInt[1]), roomNum);
-							roomNum++;
+                  //creates node and puts it into array
+                     nodeArray[roomNum] = new Node (Integer.parseInt(stringsOfInt[1]),roomNum);
+                     roomNum++;
                   }
-						}
-						else{
-						for (roomNum = 0; roomNum < (k*k); roomNum++){
-						nodeArray[roomNum].sharesWallWith(roomNum + 1);
-						nodeArray[roomNum].sharesWallWtih(roomNum - 1);
-						nodeArray[roomNum].sharesWallWith(roomNum + k);
-						nodeArray[roomNum].sharesWallWith(roomNum + k - 1);
-						nodeArray[roomNum].sharesWallWith(roomNum - k);
-						nodeArray[roomNum].sharesWallWith(roomNum - k + 1);}
-			}
-               
-         		
-            
-            
+               }
+               if (scaryRoomNum < (k*k)){
+                  nodeArray[scaryRoomNum].setScary(Integer.parseInt(stringsOfInt[1]));
+                  nodeArray[scaryRoomNum].setWork(Integer.parseInt(stringsOfInt[1]));//TEMPORARY
+                  scaryRoomNum++;
+               }
+            }
+         
+            for (roomNum = 0; roomNum < (k*k); roomNum++){//gets shared walls
+               nodeArray[roomNum].sharesWallWith(roomNum + 1, k);
+               nodeArray[roomNum].sharesWallWtih(roomNum - 1, k);
+               nodeArray[roomNum].sharesWallWith(roomNum + k, k);
+               nodeArray[roomNum].sharesWallWith(roomNum + k - 1, k );
+               nodeArray[roomNum].sharesWallWith(roomNum - k, k);
+               nodeArray[roomNum].sharesWallWith(roomNum - k + 1, k);}
+                
          }
             catch (Exception e){System.err.println("Error " + e);}
-      
       }
-   
-   
-   
    
       public static void main (String[] args) throws Exception {
          Driver3 d3 = new Driver3();
