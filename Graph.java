406cb3da02162fe59422ced85e2e1bea15db452e
austin123 <zam449@hotmail.com>
2013-04-07 12:49:42 -0700

    outline of graph class

---
+++ Graph.java
@@ -0,0 +1,42 @@
+   public class Graph{
+   
+      public void blockWall(Node a, Node b){
+      
+      }
+   
+   
+      public int[] calculateRoomsReachable(Node nd){
+         int[] avaliableRooms = null;
+      
+         return avaliableRooms;
+      }
+   
+   
+      public int[] minWorkForAllRoomsAccessible(Node nd){
+         int[] minDoorWork = null;
+      
+         return minDoorWork;
+      }
+   
+      public int[] leastWorkOpenDoors(Node nd1, Node nd2){
+         int[] minDoorWorkBetweenRooms = null;
+      
+         return minDoorWorkBetweenRooms;
+      }
+   
+      public int totalSpookinessBetweenRooms(Node nd1, Node nd2){
+         int totalSpookiness = 0;
+      
+         return totalSpookiness;
+      }
+   
+      public int maxSpookiness(Node nd1, Node nd2){
+         int maxSpookiness = 0;
+      
+         return maxSpookiness;
+      }
+   
+      public void checkEdge(Node nd){
+      
+      }
+   }
