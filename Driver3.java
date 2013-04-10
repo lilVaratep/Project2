49cbcc0fb24e6fe11eda4df886b2e63a04bc3cc9
austin123 <zam449@hotmail.com>
2013-04-10 01:20:18 -0700

    worked on 3 methods
    1. checkIfIsEdge sends room numbers to checkEdge in theGraph
    2. setBlockedWalls sends room Nodes to blockWall in theGraph
    3 calcReachableRooms only does stuff in Driver03(which ill move some stuff to theGraph) and so far works for nogas picture example(havent done any other testing for it yet)
    NOTE: Also, nogas file isnt the same as his example picture, so ill upload the file that is the same as his picture

--- Driver3.java
+++ Driver3.java
@@ -4,7 +4,6 @@
 
 	
    public class Driver3{
-      private Node start = null;
       private File txtFile;
       private String fileName, line;
       private boolean kFound = false;
@@ -12,6 +11,8 @@
       private int k = 2; 
       private ArrayList<Integer> roomsWithBlockedWalls = new ArrayList<Integer>();
       private ArrayList<Node> nodeArray = new ArrayList<Node>();
+      private ArrayList<Integer> reachableRooms = new ArrayList<Integer>(); 
+      private int aRoom = 0;
    	
       public void readFile(){
          int scaryRoomNum, arrayPos, roomNum= 0;
@@ -43,16 +44,28 @@
             catch (Exception e){System.err.println("Error " + e);}
          checkIfIsEdge();
          setBlockedWalls();
-         calcReachableRooms(k);
-         calcMinWorkForTotalAccess(k);
-         calcMinWorkBetweenRooms();
-         calcSpookinessBetweenRooms();
-         calcMaxSpookiness();
+         calcReachableRooms();
+        // calcMinWorkForTotalAccess();
+        // calcMinWorkBetweenRooms();
+        // calcSpookinessBetweenRooms();
+        // calcMaxSpookiness();
       }
    	
       private void checkIfIsEdge(){
-         for (int i = 1; i < nodeArray.size();i++)
-            theGraph.checkEdge(nodeArray.get(i));
+         int specificRoomNum, accessibleRoom;
+         int v = 0;
+         for (int i = 0; i < nodeArray.size();i++){//for each node
+            specificRoomNum = nodeArray.get(i).getRoomNumber();
+            v = 0;
+            while (v < nodeArray.get(i).getRoomsAccessible().size()){
+               accessibleRoom = nodeArray.get(i).getRoomsAccessible().get(v);
+               if (theGraph.checkEdge(specificRoomNum, accessibleRoom, k) == true){
+                  nodeArray.get(i).getRoomsAccessible().remove(v);
+                  nodeArray.get(i).getRoomsAccessible().trimToSize();}
+               else
+                  v++;
+            }
+         }
       }
    	
       private void setBlockedWalls(){
@@ -65,15 +78,50 @@
             secondRoom = nodeArray.get(secondRoomNum);//gets node
             theGraph.blockWall(firstRoom,secondRoom);}
       }
-   	
-      private void calcReachableRooms(int k){
-         int [] reachableRooms = new int[k*k];//in case all the rooms are reachable
-         reachableRooms = theGraph.calculateRoomsReachable(nodeArray.get(0));
-         for (int a = 0; a < reachableRooms.length; a++)
-            System.out.print(reachableRooms[a]);
+   
+      private void calcReachableRooms(){//gets a room and sees if there is a path to starting room
+         Node room, startRoom;
+         int adjacentRoom;
+         reachableRooms.add(nodeArray.get(0).getRoomNumber());// . . . . . . . . . . . .room 0 always first starting room
+         for (int y = 1; y < nodeArray.size(); y++){//                                  for each room
+            room = nodeArray.get(y);// . . . . . . . . . . . . . . . . . . . . . . . . .set as starting room
+            startRoom = room;//                                                         save starting room to possibly add to array
+            while (room != nodeArray.get(0)){//. . . . . . . . . . . . . . . . . . . . .while we have not gotten to room 0
+               for(int adjRm = 0; adjRm < room.getRoomsAccessible().size(); adjRm++){// for each room accessible
+                  int adjacentRoomNum = room.getRoomsAccessible().get(adjRm);// . . . . get an accessible room number
+                  for(int rchRmNum = 0; rchRmNum < reachableRooms.size(); rchRmNum++){//for each room that -> room 0
+                     int reachableRoomNum = reachableRooms.get(rchRmNum);// . . . . . . get a specific reachable room
+                     if (adjacentRoomNum == reachableRoomNum){//								 if the room has an adjacent room number that has a path to room 0
+                        room = nodeArray.get(0);// . . . . . . . . . . . . . . . . . . .room is reachable(path to room 0)
+                        break;}//got to room 0
+                  }
+                  if(room == nodeArray.get(0))
+                     break;
+               }
+            	
+               if(room == nodeArray.get(0))
+                  break;
+               else{
+                  int w = 0;
+                  Node lowAdjRoom = nodeArray.get(room.getRoomsAccessible().get(w));
+                  if (lowAdjRoom.getPrevRoom() == room.getRoomNumber())
+                     break;
+                  else{
+                     lowAdjRoom.setPrevRoom(room.getRoomNumber());
+                     room = lowAdjRoom;}
+               }
+            }
+            if (room == nodeArray.get(0))
+               reachableRooms.add(startRoom.getRoomNumber());
+         }
+               			
+         
+         for (int b = 0; b <reachableRooms.size();b++)
+            System.out.print(reachableRooms.get(b) + ", ");
+         
       }
    	
-      private void calcMinWorkForTotalAccess(int k){
+      private void calcMinWorkForTotalAccess(){
          int [] minWork = new int[k];// not sure if long enough
          minWork = theGraph.minWorkForAllRoomsAccessible(nodeArray.get(0));
          for (int a = 0; a < minWork.length; a++)
