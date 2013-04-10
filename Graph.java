36b4b7e5ce320c001845b6e6e8207b0cd7ef96cc
austin123 <zam449@hotmail.com>
2013-04-10 01:10:58 -0700

    worked on some methods
    blockWall blocks certain walls between rooms, given by the file
    calculateRoomsReachable is not being used right now
    checkEdge deletes room nums that are out of bounds, or that side nodes cant get to

--- Graph.java
+++ Graph.java
@@ -1,13 +1,38 @@
+   import java.util.*;
+  
    public class Graph{
+      int wallsPerRoom = 6;
+      private ArrayList<Integer> avaliableRooms = new ArrayList<Integer>();
    
       public void blockWall(Node a, Node b){
-      
-      }
+         ArrayList<Integer> adjRooms = new ArrayList<Integer>();
+         int i = 0;
+         adjRooms = a.getRoomsAccessible();
+         while (i < adjRooms.size())
+            if (adjRooms.get(i) == b.getRoomNumber()) 
+               a.getRoomsAccessible().remove(i);
+            else
+               i++;
+         a.getRoomsAccessible().trimToSize();
+         		
+         i = 0;
+         adjRooms = b.getRoomsAccessible();
+         while (i < adjRooms.size())
+            if (adjRooms.get(i) == a.getRoomNumber()) 
+               b.getRoomsAccessible().remove(i);
+            else
+               i++;
+         a.getRoomsAccessible().trimToSize();
+      }   
    
-   
-      public int[] calculateRoomsReachable(Node nd){
-         int[] avaliableRooms = null;
-      
+      public ArrayList calculateRoomsReachable(int adjacentRoom){
+         boolean notInArray = true;
+         for (int g = 0; g < avaliableRooms.size(); g++)
+            if (avaliableRooms.get(g) == adjacentRoom){
+               notInArray = false;
+               break;}
+         if (notInArray == true)
+            avaliableRooms.add(adjacentRoom);
          return avaliableRooms;
       }
    
@@ -36,7 +61,26 @@
          return maxSpookiness;
       }
    
-      public void checkEdge(Node nd){
-      
+      public boolean checkEdge(int aRoomNum, int anAccessibleRoom, int k){
+         if (anAccessibleRoom < 0)//outside low range
+            return true;
+         else if (anAccessibleRoom > (k*k))//outside high range
+            return true;
+         else if (aRoomNum % k == 0){//left edge room
+            if (anAccessibleRoom == aRoomNum + k - 1)
+               return true;
+            else if (anAccessibleRoom == aRoomNum - 1)
+               return true;
+            else
+               return false;}
+         else if ((aRoomNum + 1) % k == 0){//right edge room
+            if (anAccessibleRoom == aRoomNum - k + 1)
+               return true;
+            else if (anAccessibleRoom == aRoomNum + 1)
+               return true;
+            else
+               return false;}
+         else
+            return false;
       }
    }
